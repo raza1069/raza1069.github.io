85ea5d2a9f98bb1c5312279c1f93c163d0ef6e6c
raza1069 <43216060+raza1069@users.noreply.github.com>
2018-11-11 23:03:54 -0600

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,64 +1,45 @@
-/*
-Gameplan :
-
-HTML
-(*) Calculator Window foe operstion to be shown along with results
-() Number buttons for the caculator
-() Operator buttons (+. -, /, *, Etc)
-() '=' button
-() Reset or Clear button
-
-
-Javascript:
-()  Function that captures what wa typed in the calculatorWindow
-() function that will add numbers, subtract, etc
- */
 function calculate(expression)
 {
-    //expression is a STRING of everything in your calculatorWindow
-    // We need to EXTRACT  the first number from the 'expression'
-    // expression = 1+2
+    if (expression.substr(1, 1) === '+')
+    {
+        var firstNum = Number(expression.substr(0, 1));
+        var operator = expression.substr(1, 1); //capture the '+'
+        var secondNum = Number(expression.substr(2, 2));
+    }
+    else if (expression.substr(1,1) === '-')
+    {
+        var firstNum = Number(expression.substr(0, 1));
+        var operator = expression.substr(1, 1); //capture the '+'
+        var secondNum = Number(expression.substr(2, 2));
+    }
+    else if (expression.substr(1,1) === '*')
+    {
+        var firstNum = Number(expression.substr(0, 1));
+        var operator = expression.substr(1, 1); //capture the '+'
+        var secondNum = Number(expression.substr(2, 2));
+    }
 
+    else if (expression.substr(1,1) === '/')
+    {
+        var firstNum = Number(expression.substr(0, 1));
+        var operator = expression.substr(1, 1); //capture the '+'
+        var secondNum = Number(expression.substr(2, 2));
+    }
+    else if (expression.substr(1,1) === '^')
+    {
+        var firstNum = Number(expression.substr(0, 1));
+        var operator = expression.substr(1, 1); //capture the '+'
+        var secondNum = Number(expression.substr(2, 2));
+    }
 
-    var firstNum = Number(expression.substr(0, 2));
-    var operator = expression.substr(2, 1); // capture the '+'
-    var secondNum = Number(expression.substr(3, 2));
-
-    if(expression.substr(2,1)=== '+')
+    else
     {
         var firstNum = Number(expression.substr(0, 2));
-        var operator = expression.substr(2, 1); // capture the '+'
-        var secondNum = Number(expression.substr(3, 2));
-    }
-    else if(expression.substr(2,1)=== '-')
-    {
-        var firstNum = Number(expression.substr(0, 2));
-        var operator = expression.substr(2, 1); // capture the '+'
-        var secondNum = Number(expression.substr(3, 2));
-    }
-    else if(expression.substr(2,1)=== '/')
-    {
-        var firstNum = Number(expression.substr(0, 2));
-        var operator = expression.substr(2, 1); // capture the '+'
-        var secondNum = Number(expression.substr(3, 2));
-    }
-    else if(expression.substr(2,1)=== '*')
-    {
-        var firstNum = Number(expression.substr(0, 2));
-        var operator = expression.substr(2, 1); // capture the '+'
-        var secondNum = Number(expression.substr(3, 2));
-    }
-    else if(expression.substr(2,1)=== '^')
-    {
-        var firstNum = Number(expression.substr(0, 2));
-        var operator = expression.substr(2, 1); // capture the '+'
+        var operator = expression.substr(2, 1); //capture the '+'
         var secondNum = Number(expression.substr(3, 2));
     }
 
 
-    var firstNum = Number(expression.substr(0, 1));
-    var operator = expression.substr(1, 1); // capture the '+'
-    var secondNum = Number(expression.substr(2, 2));
 
 
 
@@ -66,51 +47,57 @@ function calculate(expression)
 
 
 
-    if (operator === '+')// that means I add firstnum and second num
+
+
+
+
+    if(operator === '+')//that means i add firstNum and secondNum
     {
-        return addNums(firstNum, secondNum);
-    }
-    else if (operator === '-')
-    {
-        return subtractNums(firstNum, secondNum)
-    }
-    else if (operator === '/')
-    {
-        return divideNums(firstNum, secondNum)
+        return addNums(firstNum, secondNum); //returns the result of calling 'addnums'
+        //after sending it firstNum and secondNum
     }
     else if (operator === '*')
     {
-        return multiplyNums(firstNum, secondNum)
+        return multiplyNums(firstNum, secondNum);
+        //call subtract nums, etc.
+    }
+    else if (operator === '-')
+    {
+        return subtractNums(firstNum, secondNum);
+    }
+    else if (operator === '/')
+    {
+        return divideNums(firstNum, secondNum);
     }
     else if (operator === '^')
     {
-        return squareNums(firstNum, secondNum)
+        return squareNums(firstNum, secondNum);
     }
 
-
-
-
-
-
 }
 
-function addNums(p1, p2)
+
+
+function addNums(p1,p2)
 {
+    //p1 and p2 are sent to addNums... to do this:
     return p1 + p2;
+
 }
-function subtractNums(p1, p2)
-{
-    return p1 - p2;
-}
-function divideNums(p1, p2)
-{
-    return p1 / p2;
-}
-function multiplyNums(p1, p2)
+function multiplyNums(p1,p2)
 {
     return p1 * p2;
+
 }
-function squareNums(p1, p2)
+function subtractNums(p1,p2)
 {
-    return Math.pow(p1 , p2);
+    return p1 - p2
+}
+function divideNums(p1,p2)
+{
+    return p1 / p2
+}
+function squareNums(p1,p2)
+{
+    return Math.pow(p1,p2)
 }
